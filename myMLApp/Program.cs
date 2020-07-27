4d58df101baee2e719f937ffefb7dfa3472d5c07
Ave_Nocturna <heavymusic75@gmail.com>
2020-07-27 19:02:03 -0400

    Agregar archivos de proyecto.

---
+++ myMLApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MyMLAppML.Model;
+
+namespace myMLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Leave a Comment,Please :");
+             
+            string Coment;
+            Coment = Console.ReadLine();//Get a comment from the user.
+            var input = new ModelInput();
+            input.SentimentText = Coment;//Give the comment left as input
+
+            // Load model and predict output from the comment left
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}\n");
+        }
+    }
+}
